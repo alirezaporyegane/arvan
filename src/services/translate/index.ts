6e5ca6491de53c87d0c33cd25cc8e56beb5d189a
alirezaporyegane <alirezaporyegane@yahoo.com>
2024-11-12 22:56:05 +0330

    feat: Add Translate

--- src/services/translate/index.ts
+++ src/services/translate/index.ts
@@ -1,5 +1,19 @@
+import { createI18n } from 'vue-i18n'
+import { en as enVuetify } from 'vuetify/locale'
 import en from './en'
 
-export default {
-  en
-}
+const i18n = createI18n({
+  locale: 'fa',
+  fallbackLocale: 'en',
+  legacy: false, // Vuetify does not support the legacy mode of vue-i18n
+  messages: {
+    en: {
+      ...en,
+      $vuetify: {
+        ...enVuetify
+      }
+    }
+  }
+})
+
+export default i18n
